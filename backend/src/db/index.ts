f1d4c247a043821653a2b61f3ef699d14f45779c
Omaya404 <omaya.battour@gmail.com>
2026-01-13 16:47:07 +0100

    docker setup for backend and db done

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,8 +1,13 @@
 import { DataSource } from "typeorm";
+import env from "../env";
 
 export default new DataSource({
-  type: "sqlite",
-  database: "src/db/db.sqlite",
+  type: "postgres",
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USER,
+  password: env.DB_PASS,
   entities: ["src/entities/*.ts"],
   synchronize: true,
   //logging: true
