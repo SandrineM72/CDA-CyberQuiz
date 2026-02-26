ec2036a62d6ecadd88c9b2b5708c2425160a009a
SandrineM72 <sandrine.maingourd.pro72@gmail.com>
2026-02-26 11:30:59 +0100

    Modif to trigger workflow

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-center py-5 border-t-4 border-[#00bb0d]">
-      <p className="text-[10px] text-[#a5a5a5] tracking-widest">ALL RIGHTS RESERVED © 2026</p>
+      <p className="text-[10px] text-[#a5a5a5] tracking-widest">Tous droits réservés © 2026</p>
     </footer>
   );
 }
