ec7b6516c4c1ba47c98987c8e788653036942230
SandrineM72 <sandrine.maingourd.pro72@gmail.com>
2026-03-10 12:31:48 +0100

    fix: correct GraphQL import for ESM compatibility

--- backend/src/entities/errors.ts
+++ backend/src/entities/errors.ts
@@ -1,4 +1,4 @@
-import { GraphQLError } from "graphql/error"
+import { GraphQLError } from "graphql"
 
 export class UnauthenticatedError extends GraphQLError {
     constructor(params?: { message?: string}) {
