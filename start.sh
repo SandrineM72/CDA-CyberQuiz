73211d53cdd013a0f6a68b2dcedfda32b833565f
SandrineM72 <sandrine.maingourd.pro72@gmail.com>
2026-04-25 18:38:59 +0200

    fixed start.sh for deployment in production

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEPLOY_ENV=${DEPLOY_ENV:-staging}
+export DEPLOY_ENV=${DEPLOY_ENV:-production}
 
 docker compose -p "$DEPLOY_ENV" -f docker-compose.prod.yml --env-file .env.production down && \
 docker compose -p "$DEPLOY_ENV" -f docker-compose.prod.yml --env-file .env.production pull && \
